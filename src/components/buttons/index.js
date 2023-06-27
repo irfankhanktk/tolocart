dafcfa807e8e38f79460d8fc2b2dcb3d56512490
irfankhanktk <irfan.khan93093@gmail.com>
2023-06-27 07:14:33 +0500

    fixed conflicts

--- src/components/buttons/index.js
+++ src/components/buttons/index.js
@@ -5,24 +5,35 @@ export const PrimaryButton = ({
   onClick,
   ...props
 }) => {
-<<<<<<< HEAD
-    return (
-        <button onClick={onClick} type="button" class="btn mb-2 read-more-btn" style={{ background: '#FF5A00', color: '#fff', fontSize:'20px', fontFamily:'Gilroy', fontWeight:'600', lineHeight:'29px',  ...style }} {...props}>
-            {title}
-        </button>
-    );
-};
-=======
   return (
     <button
       onClick={onClick}
       type="button"
-      class="btn mb-2"
-      style={{ background: "#FF5A00", color: "#fff", ...style }}
+      class="btn mb-2 read-more-btn"
+      style={{
+        background: "#FF5A00",
+        color: "#fff",
+        fontSize: "20px",
+        fontFamily: "Gilroy",
+        fontWeight: "600",
+        lineHeight: "29px",
+        ...style,
+      }}
       {...props}
     >
       {title}
     </button>
   );
+
+  // return (
+  //   <button
+  //     onClick={onClick}
+  //     type="button"
+  //     class="btn mb-2"
+  //     style={{ background: "#FF5A00", color: "#fff", ...style }}
+  //     {...props}
+  //   >
+  //     {title}
+  //   </button>
+  // );
 };
->>>>>>> c15e5d434456c91b326dff159b2dba86d9bd5e76
